21f21c7638bc6d90695be0e31babad9f238c35b0
barfa1 <barfashubham1@gmail.com>
2021-09-23 03:47:27 +0000

    Revert back some changes and setting head to c03cd4d this

---
+++ src/test/CalculateJava.java
@@ -0,0 +1,13 @@
+package test;
+
+public class CalculateJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Calc Java");
+		Integer aa = 20;
+		System.out.println("aa is "+aa);
+
+	}
+
+}
